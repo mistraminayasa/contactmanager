b95b34b88e9ec269e3eb79be0b420f2129ccc4da
Mistra <manoyasamistra@gmail.com>
2019-02-24 14:19:13 +0700

    Updates

---
+++ precache-manifest.6ea4ec7826910816a6c0cdc6f413f95e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/mistraminayasa/contactmanager/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "2cb6efccc8ab0390f84c",
+    "url": "/mistraminayasa/contactmanager/static/css/main.4990c529.chunk.css"
+  },
+  {
+    "revision": "fd1acf525c32a8f65646",
+    "url": "/mistraminayasa/contactmanager/static/js/runtime~main.fd1acf52.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/mistraminayasa/contactmanager/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "ddd88d92c1aa5b8ea0e7",
+    "url": "/mistraminayasa/contactmanager/static/js/2.ddd88d92.chunk.js"
+  },
+  {
+    "revision": "2cb6efccc8ab0390f84c",
+    "url": "/mistraminayasa/contactmanager/static/js/main.2cb6efcc.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/mistraminayasa/contactmanager/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/mistraminayasa/contactmanager/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/mistraminayasa/contactmanager/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "ddd88d92c1aa5b8ea0e7",
+    "url": "/mistraminayasa/contactmanager/static/css/2.2d7dd724.chunk.css"
+  },
+  {
+    "revision": "7384b65d8378270032c490fd4b45f179",
+    "url": "/mistraminayasa/contactmanager/index.html"
+  }
+];
